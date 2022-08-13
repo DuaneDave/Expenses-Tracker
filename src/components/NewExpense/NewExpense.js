d58bc36fabcf63823b919faffd7eb9d503b73328
Duane David <duanedave17@gmail.com>
2022-08-13 16:02:29 -0700

    new update

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,13 +4,13 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
-  
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+
+    props.onAddExpense(expenseData);
   };
 
   return (
